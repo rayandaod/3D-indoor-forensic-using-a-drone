53392da77b211594412ea713f1b8885574cc20b5
Rayan DAOD <rayan.daodnathoo@epfl.ch>
2018-03-09 16:49:21 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ZED_Spatial_Mapping.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_Spatial_Mapping.dir/src/GLObject.o"
+  "CMakeFiles/ZED_Spatial_Mapping.dir/src/Shader.o"
+  "CMakeFiles/ZED_Spatial_Mapping.dir/src/main.o"
+  "ZED_Spatial_Mapping.pdb"
+  "ZED_Spatial_Mapping"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_Spatial_Mapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/spatial_mapping.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for spatial_mapping.
+# This may be replaced when dependencies are built.
